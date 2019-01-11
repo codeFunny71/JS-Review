d1dd8bd968c2f8a31b938467c59985e1123e1e0d
Marcus Absher <mabsher@mail.greenriver.edu>
2019-01-11 18:13:39 -0500

    Created FizzBuzz program

--- js-review.js
+++ js-review.js
@@ -1,10 +1,39 @@
 
-console.log("\"Introduction to JavaScript\"")
+console.log("\"Introduction to JavaScript\"");
 
-console.log("FizzBuzz Program")
+console.log("FizzBuzz Program");
 for (let i = 0; i < 100 ; i++) {
-    if ((i+1) % 3 == 0) console.log("Fizz")
-    else if (i+1 % 5 == 0) console.log("Buzz")
-    else if (i+1 % 3 == 0 && i+1 % 5 == 0) console.log("Buzz")
+    if (((i+1) % 3 === 0) && !((i+1) % 5 === 0)) console.log("Fizz");
+    else if (((i+1) % 5 === 0) && !((i+1) % 3 === 0)) console.log("Buzz");
+    else if (((i+1) % 3 ===0) && ((i+1) % 5 === 0)) console.log("Buzz");
     else console.log(i)
 }
+// for (let i = 0; i < 100 ; i++) {
+//     let d = a => (a % 3) === 0;
+//     let e = a => (a % 5) === 0;
+//     switch (i) {
+//         case d && !e:
+//             console.log("Fizz");
+//             break;
+//         case e && !d:
+//             console.log("Buzz");
+//             break;
+//         case d && e:
+//             console.log("FizzBuzz");
+//             break;
+//         default:
+//             console.log(i);
+//     }
+// }
+console.log("Count \"B\'s\" Program");
+
+const countBs = (bString) => console.log((bString.match(new RegExp("B", "g")) || []).length);
+test = "Big boys buy Big Belly burgers!";
+countBs(test);
+
+const  countChar = (mainStr, CharToCount) => {
+   console.log(mainStr.split(CharToCount).length - 1)
+};
+
+console.log("Count \'selected char\' Program");
+countChar(test, "y");
