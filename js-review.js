54f22176260302d4a098d3fec1665892b9f881ea
Marcus Absher <mabsher@mail.greenriver.edu>
2019-01-11 18:32:29 -0500

    added fixed fizzbuzz and counting b programs

--- js-review.js
+++ js-review.js
@@ -2,10 +2,10 @@
 console.log("\"Introduction to JavaScript\"");
 
 console.log("FizzBuzz Program");
-for (let i = 0; i < 100 ; i++) {
-    if (((i+1) % 3 === 0) && !((i+1) % 5 === 0)) console.log("Fizz");
-    else if (((i+1) % 5 === 0) && !((i+1) % 3 === 0)) console.log("Buzz");
-    else if (((i+1) % 3 ===0) && ((i+1) % 5 === 0)) console.log("Buzz");
+for (let i = 1; i < 101 ; i++) {
+    if (i % 3 == 0 && (i % 5 == 0)) console.log("FizzBuzz");
+    else if (i % 3 == 0 && !(i % 5 == 0)) console.log("Fizz");
+    else if (i % 5 == 0 && !(i % 3 == 0)) console.log("Buzz");
     else console.log(i)
 }
 // for (let i = 0; i < 100 ; i++) {
